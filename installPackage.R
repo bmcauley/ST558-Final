68cdfe4e31043782b6afa173bd9cf3336c67cd56
Bennett McAuley <bennettmcauley@gmail.com>
2022-11-19 18:55:09 -0500

    Script for checking and loading packages

---
+++ installPackage.R
@@ -0,0 +1,14 @@
+# Run this script to install and/or load the required packages for the STFP application
+
+# Packages required for the app
+pkglst <- c("shiny", "tidyverse", "caret", "leaps", "gbm")
+
+OK <- pkglst %in% rownames(installed.packages())
+
+# If any of the packages in pkglst are not installed, install them
+if (any(okPkg == FALSE)) {
+  install.packages(pkglst[!OK])
+}
+
+# Load the packages
+invisible(lapply(pkglst, require, character.only = TRUE))
